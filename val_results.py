c8dcf3e163d6ab09412a000bf4228b67ba4975d0
Sreenidhi <sreenidhipuppala2@gmail.com>
2026-08-05 13:38:36 +0530

    Delete val_results.py

--- val_results.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load your best trained model
-model = YOLO('runs/detect/train3/weights/best.pt')
-
-# Validate the model and save class-wise metrics to JSON
-model.val(save_json=True, data='data.yaml')
